16ee4f86148840b926c9b03b2f73022f2531592a
Daniel Rivero <danielrivero2000@gmail.com>
2024-12-09 18:57:53 -0400

    [feat] solves merge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,9 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-<<<<<<< HEAD
 import Script from "next/script";
 import { GoogleAnalytics } from '@next/third-parties/google'
-=======
 import SmoothScrolling from "@/components/ui/smooth-scrollet";
->>>>>>> dev
 
 const fontSans = FontSans({
   subsets: ["latin"],
