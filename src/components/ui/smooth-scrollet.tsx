321d4c99386cf6eddf9aec6deaaf34401535cc42
Daniel Rivero <danielrivero2000@gmail.com>
2024-12-09 19:06:25 -0400

    [fix] solves lenis property

--- src/components/ui/smooth-scrollet.tsx
+++ src/components/ui/smooth-scrollet.tsx
@@ -3,7 +3,7 @@ import { ReactLenis } from "@studio-freight/react-lenis";
 
 function SmoothScrolling({ children }:any) {
   return (
-    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothTouch: true }}>
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
       {children}
     </ReactLenis>
   );
