6d86d3c1544cce76d741f14a99e2d52af437084c
Daniel Rivero <danielrivero2000@gmail.com>
2024-12-09 18:53:21 -0400

    [feat] adds smoothscroll with lenis and gsap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import SmoothScrolling from "@/components/ui/smooth-scrollet";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -63,7 +64,10 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="dark">
           <TooltipProvider delayDuration={0}>
-            {children}
+            <SmoothScrolling>
+
+              {children}
+            </SmoothScrolling>
             <Navbar />
           </TooltipProvider>
         </ThemeProvider>
