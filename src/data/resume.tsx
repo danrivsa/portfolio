589749727be1a094f441531574a945bf0008cfe7
Daniel Rivero <danielrivero2000@gmail.com>
2026-07-26 14:06:18 -0400

    feat: update resume details including job titles and links

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -12,7 +12,7 @@ export const DATA = {
   description:
     "Software engineer and AI enthusiast. I love building innovative solutions and explore new tech trends",
   summary:
-    "I'm a motivated software engineer with a passion for building innovative solutions. I'm skilled in [architecting robust cloud solutions on AWS and leveraging state-of-the-art technologies](/#skills). With a [background in computer science](/#education), I bring a unique perspective to problem-solving and software development. I also have experience in game development through [numerous game jams i participated just for fun](/#gamejams).",
+    "I'm a motivated software engineer with a passion for building innovative solutions. I'm skilled in [architecting robust cloud solutions on AWS and leveraging state-of-the-art technologies](/portfolio/#skills). With a [background in computer science](/portfolio/#education), I bring a unique perspective to problem-solving and software development. I also have experience in game development through [numerous game jams i participated just for fun](/portfolio/#gamejams).",
   avatarUrl: "./me.png",
   languages: [
     {
@@ -222,22 +222,22 @@ export const DATA = {
       href: "https://mxglobal.center/",
       badges: [],
       location: "Remote",
-      title: "Sr. Software Engineer",
+      title: "Software Engineer",
       logoUrl: "./mx-global.png",
-      start: "Jul 2023",
+      start: "Oct 2024",
       end: undefined,
       description:
-        "As a Senior Software Engineer at MX Global Center, I focused on enhancing operational efficiency and streamlining processes. I was instrumental in maintaining and optimizing critical infrastructure components, implementing innovative software solutions and APIs to automate repetitive tasks. I leveraged my expertise in Angular and Xano to develop custom applications, including an ETL process to automate manual journal calculations. By automating these tasks, I significantly reduced operational costs, increased accuracy, and improved overall productivity.",
+        "As a Software Engineer at MX Global Center, I focused on enhancing operational efficiency and streamlining processes. I was instrumental in maintaining and optimizing critical infrastructure components, implementing innovative software solutions and APIs to automate repetitive tasks. I leveraged my expertise in Angular and Xano to develop custom applications, including an ETL process to automate manual journal calculations. By automating these tasks, I significantly reduced operational costs, increased accuracy, and improved overall productivity.",
     },
     {
       company: "Newtoms LLC",
       badges: [],
       href: "https://www.newtoms.com/",
       location: "Remote",
-      title: "Software Engineer and Cloud Solutions Architect",
+      title: "Jr. Software Developer and Cloud Solutions Architect",
       logoUrl: "./newtoms.png",
       start: "Jan 2023",
-      end: undefined,
+      end: "Apr 2025",
       description:
         "During my time at Newtoms LLC, I was involved in the development of innovative products, including Avatar X and Virtual Staging. I utilized Angular and AWS to build scalable and efficient solutions. For my thesis project, I architected a semi-real-time smart surveillance system that leveraged AWS services like Rekognition Custom Labels, Kinesis Video Streams, SageMaker, S3, CloudFront, API Gateway, Cognito, AppSync, Amplify, and a GraphQL API to detect and analyze incidents in real-time. The system incorporated computer vision and audio analysis techniques to identify persons, weapons, fires, and gunshots. Real-time notifications were delivered using AppSync, and the processing of video streams was streamlined through a serverless architecture utilizing multiple Lambda functions and event-driven communication via SQS and SNS. Docker containers were used to handle image processing with OpenCV and to connect the camera to the AWS cloud.",
     },
@@ -246,7 +246,7 @@ export const DATA = {
       href: "https://www.pwc.com/ve/es.html",
       badges: [],
       location: "Hybrid",
-      title: "Assocciate (RAS) and Jr. Software Engineer",
+      title: "Assocciate (RAS) and Jr. Software Developer",
       logoUrl: "./pwc.png",
       start: "Mar 2021",
       end: "Dec 2022",
