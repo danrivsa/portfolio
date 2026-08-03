92ac264b3499d0d11031b88f0ae3581c355d2014
Daniel Rivero <danielrivero2000@gmail.com>
2026-08-03 19:00:39 -0400

    add new export

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(date: string) {
+  if (!date.includes("T")) {
+    date = `${date}T00:00:00`;
+  }
+  const targetDate = new Date(date);
+
+  return targetDate.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+}
